0f3e741d4164838163d990cf2ea0e86f945e12a7
Jason <jasonbushis@gmail.com>
2014-07-10 15:26:08 -0700

    employee controller and partial modified

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -75,6 +75,6 @@ class EmployeesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :primary_location_id, :secondary_location_id, :badge, :department_id, :muster_location_id, :gender)
+      params.require(:employee).permit(:first_name, :last_name, :location_id, :badge, :department_id, :muster_location_id, :gender)
     end
 end
