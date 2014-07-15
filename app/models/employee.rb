b49c5eb7aace20f37e2cbe56b677a3d71558be67
Jason <jasonbushis@gmail.com>
2014-07-15 12:08:00 -0500

    modified employee import - added seed data for departments

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,10 +7,22 @@ class Employee < ActiveRecord::Base
   		"#{self.first_name} #{self.last_name}"
 	end
 
-	def self.import(file)  
-      	CSV.foreach(file.path, headers: true) do |row|  
-      	Employee.create! row.to_hash  
-    end  
+	#def self.import(file)  
+     # 	CSV.foreach(file.path, headers: true) do |row|  
+     # 	Employee.create! row.to_hash  
+    #end  
+
+    def self.import(file)  
+      	CSV.foreach(file.path, headers: false) do |row|  
+      	e = Employee.new
+      	e.last_name = row[0]
+      	e.first_name = row[1] 
+      	e.badge = row[2]
+      	e.gender = row[3]
+      	e.muster_location = MusterLocation.find_by_number("#{row[4]}")
+      	e.location = Location.find_by_number("#{row[5]}")
+      	e.save!
+      end
     end  
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,52 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
+#the following seed data is needed for the application
 MusterLocation.delete_all
-MusterLocation.create!(:name => 'Muster Area 1', :number => '1')
-MusterLocation.create!(:name => 'Muster Area 2', :number => '2')
+MusterLocation.create!(name: 'Muster Area 1', number: '1')
+MusterLocation.create!(name: 'Muster Area 2', number: '2')
+puts 'Muster Locations Created'
+
+Department.delete_all
+Department.create!(number: '11', name: 'Lab')
+Department.create!(number: '13', name: 'Shop-Mach')
+Department.create!(number: '14', name: 'Shop-Assy')
+Department.create!(number: '15', name: 'Urethane')
+Department.create!(number: '20', name: 'Sales')
+Department.create!(number: '25', name: 'Shipping')
+Department.create!(number: '30', name: 'Office Staff')
+Department.create!(number: '31', name: 'Analysis')
+Department.create!(number: '32', name: 'Programmers')
+Department.create!(number: '33', name: 'IT')
+Department.create!(number: '40', name: 'Executives')
+Department.create!(number: '45', name: 'Operations')
+Department.create!(number: '50', name: 'Engineering')
+Department.create!(number: '60', name: 'Technicians')
+puts 'Departments Created'
+
 
 Building.delete_all
-Building.create!(:number => '1', :name => 'Building One')
-Building.create!(:number => '2', :name => 'Building Two')
-Building.create!(:number => '3', :name => 'Building Three')
-puts 'Created Buildings'
+Building.create!(number: '1', name: 'Building One')
+Building.create!(number: '2', name: 'Building Two')
+Building.create!(number: '3', name: 'Building Three')
+puts 'Buildings Created'
 
 Location.delete_all
 Location.create!(number: '305', name: "Programmer's Office", building_id: "#{Building.find_by(number: '3').id}")
-Location.create!(number: '306', name: 'Opperations Office', building_id: "#{Building.find_by(number: '3').id}")
+Location.create!(number: '306', name: 'Operations Office', building_id: "#{Building.find_by(number: '3').id}")
 Location.create!(number: '307', name: 'Analysis Area', building_id: "#{Building.find_by(number: '3').id}")
 Location.create!(number: '301', name: 'Server Room', building_id: "#{Building.find_by(number: '3').id}")
 Location.create!(number: '302', name: 'Executive Office', building_id: "#{Building.find_by(number: '3').id}")
 Location.create!(number: '303', name: 'Executive Suite', building_id: "#{Building.find_by(number: '3').id}")
 
-puts 'Created Locations'
+puts 'Locations Created '
+
+
+# the seed data below is for testing
 
 Employee.delete_all
 Employee.create(first_name: 'Jason', last_name: 'Bush', badge: '347', gender: 'Male', muster_location_id: "#{MusterLocation.find_by(number: '2').id}", location_id: "#{Location.find_by(number: '302').id}", department_id: '11')
 Employee.create!(:first_name => 'John', :last_name => 'Doe', :badge => '123', :gender => 'M', :muster_location_id => '1', :location_id => '2', :department_id => '1')
 
-puts 'Created Employees'
+puts 'Employees Created'
 
